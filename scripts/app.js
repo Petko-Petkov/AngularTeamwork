63a13b910ebc1818ce4a54377eb3c26c8e201583
petko-petkov <petko.hr.petkov@gmail.com>
2014-12-28 21:58:14 +0200

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Pecata on 28.12.2014 г..
+ */
+'use strict';
+
+var addsSite = angular.module('addsSite', []);
---
+++ scripts/controllers/pageController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Pecata on 28.12.2014 г..
+ */
+
+'use strict';
+
+addsSite.controller('PageController',
+    function PageController($scope) {
+        $scope.author = "Petko Petkov";
+        $scope.year = 2014;
+    }
+);
